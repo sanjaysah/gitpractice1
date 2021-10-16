31528fe449f9f5d8e8490182c3e49681ef9e7906
sanjaysah <sanjaysah.engg@gmail.com>
2021-10-16 16:33:03 +0530

    Folder untracked file added again

---
+++ JavaOOPs/JavaOOPS/src/Basics/TypeCasting.java
@@ -0,0 +1,10 @@
+//Problem Statement - Write a Program which will take String as Input and will convert it into other primitive data types
+package Basics;
+import java.util.*;
+public class TypeCasting {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		System.out.println(input);
+	}
+}
